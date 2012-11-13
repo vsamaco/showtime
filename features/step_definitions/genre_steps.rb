b6b1282e38cab6315ce90f9da7b6213adfe705b4
Vincent Samaco <vsamaco@gmail.com>
2012-11-12 23:08:42 -0800

    moved definitions to movie steps

--- features/step_definitions/genre_steps.rb
+++ features/step_definitions/genre_steps.rb
@@ -2,18 +2,3 @@ Given /^a genre named Comedy$/ do
   @comedy = Genre.create!(:name => "Comedy")
 end
 
-When /^I create a movie Caddyshack in the Comedy genre$/ do
-  visit movies_path
-  click_link "Add Movie"
-  fill_in "Title", :with => "Caddyshack"
-  select "1980", :from => "Release year"
-  check "Comedy"
-  click_button "Save"
-end
-
-Then /^Caddyshack should be in the Comedy genre$/ do
-  visit genres_path
-  click_link "Comedy"
-  respond.should contain("1 movie")
-  respond.should contain("Caddyshack")
-end
---
+++ features/step_definitions/movie_steps.rb
@@ -0,0 +1,15 @@
+When /^I create a movie Caddyshack in the Comedy genre$/ do
+  visit movies_path
+  click_link "Add Movie"
+  fill_in "Title", :with => "Caddyshack"
+  select "1980", :from => "Release year"
+  check "Comedy"
+  click_button "Save"
+end
+
+Then /^Caddyshack should be in the Comedy genre$/ do
+  visit genres_path
+  click_link "Comedy"
+  response.should contain("1 movie")
+  response.should contain("Caddyshack")
+end
