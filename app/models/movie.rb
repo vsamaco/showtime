e5111fae5816326990d714d50c5c1f32616591a5
Vincent Samaco <vsamaco@gmail.com>
2012-11-12 19:04:25 -0800

    implemented showtime for Movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,16 @@
 class Movie < ActiveRecord::Base
   attr_accessible :showtime_date, :showtime_time
+  
+  def showtime
+    "#{formatted_date} (#{formatted_time})"
+  end
+  
+  def formatted_date
+    showtime_date.strftime("%B %d, %Y")
+  end
+  
+  def formatted_time
+    showtime_time.strftime("%l:%M%p").strip.downcase
+  end
+  
 end
